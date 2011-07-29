cc103a536a2136ec0ed52c5f0b612929cc171d87
Koen Scheres <koen_75_@hotmail.com>
2011-07-29 18:25:43 +0200

    Removed diamond & redstone from fuels

--- src/net/tweakcraft/TweakFurnace/Packages/Items.java
+++ src/net/tweakcraft/TweakFurnace/Packages/Items.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 public class Items {
 
-    private final static List<Integer> fuels = Arrays.asList(new Integer[]{Material.DIAMOND.getId(), Material.REDSTONE.getId(), Material.LOG.getId(), Material.STICK.getId(), Material.SAPLING.getId(), Material.WORKBENCH.getId(), Material.CHEST.getId(), Material.COAL.getId(), Material.BOOKSHELF.getId(), Material.WOOD.getId(), Material.LAVA_BUCKET.getId()});
+    private final static List<Integer> fuels = Arrays.asList(new Integer[]{Material.LOG.getId(), Material.STICK.getId(), Material.SAPLING.getId(), Material.WORKBENCH.getId(), Material.CHEST.getId(), Material.COAL.getId(), Material.BOOKSHELF.getId(), Material.WOOD.getId(), Material.LAVA_BUCKET.getId()});
     private final static List<Integer> smeltables = Arrays.asList(new Integer[]{Material.IRON_ORE.getId(), Material.GOLD_ORE.getId(), Material.CACTUS.getId(), Material.COBBLESTONE.getId(), Material.SAND.getId(), Material.CLAY_BALL.getId(), Material.RAW_FISH.getId(), Material.PORK.getId()});
 
     public static List<Integer> getSmeltables() {
